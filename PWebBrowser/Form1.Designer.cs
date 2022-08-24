dc0f96eabecef494d6eb79ee1036141d4191fd11
karpaff <99896884+karpaff@users.noreply.github.com>
2022-08-24 10:47:41 +0300

    First commit

---
+++ PWebBrowser/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace PWebBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.wbBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtURL.Location = new System.Drawing.Point(0, 0);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(395, 20);
+            this.txtURL.TabIndex = 1;
+            this.txtURL.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtURL_KeyPress);
+            // 
+            // wbBrowser
+            // 
+            this.wbBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wbBrowser.Location = new System.Drawing.Point(0, 20);
+            this.wbBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbBrowser.Name = "wbBrowser";
+            this.wbBrowser.Size = new System.Drawing.Size(395, 286);
+            this.wbBrowser.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 306);
+            this.Controls.Add(this.wbBrowser);
+            this.Controls.Add(this.txtURL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.WebBrowser wbBrowser;
+    }
+}
+
---
+++ PWebBrowser/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PWebBrowser
+{
+    public partial class Form1 : Form
+    {
+        private string strINIFile = "browser.ini";
+        private const string HeadWindow = "[Window]";
+        private const string HeadBrowser = "[Browser]";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Form closing event handler
+        private void Form1_FormClosed(Object sender, FormClosedEventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(new FileInfo(strINIFile).Open(FileMode.Create));
+
+            sw.WriteLine(HeadWindow);
+            sw.WriteLine("Lft=" + this.Left);
+            sw.WriteLine("Top=" + this.Top);
+            sw.WriteLine("Wdt=" + this.Width);
+            sw.WriteLine("Hgh=" + this.Height);
+            sw.WriteLine("Sta=" + this.WindowState);
+
+            sw.WriteLine(HeadBrowser);
+            sw.WriteLine("URL=" + this.wbBrowser.Url);
+
+            sw.Close();
+
+        }
+
+        // Form loading event handler
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(new FileInfo(strINIFile).Open(FileMode.Open));
+                while (!sr.EndOfStream)
+                {
+                    string firstLine = sr.ReadLine();
+                    if (firstLine == HeadWindow)
+                    {
+                        // Until you get to the end or '[' symbol
+                        while (sr.Peek() != -1 && (Convert.ToChar(sr.Peek()).CompareTo('[') != 0))
+                        {
+                            string line = sr.ReadLine();
+                            if (line != "")
+                            {
+                                string setName = line.Substring(0, 3);
+                                string setValue = line.Substring(4);
+
+                                switch (setName)
+                                {
+                                    case "Lft":
+                                        this.Left = int.Parse(setValue);
+                                        break;
+                                    case "Top":
+                                        this.Top = int.Parse(setValue);
+                                        break;
+                                    case "Wdt":
+                                        this.Width = int.Parse(setValue);
+                                        break;
+                                    case "Hgh":
+                                        this.Height = int.Parse(setValue);
+                                        break;
+                                    case "Sta":
+                                        switch (setValue)
+                                        {
+                                            case "Normal":
+                                                this.WindowState = FormWindowState.Normal;
+                                                break;
+                                            case "Minimized":
+                                                this.WindowState = FormWindowState.Minimized;
+                                                break;
+                                            case "Maximized":
+                                                this.WindowState = FormWindowState.Maximized;
+                                                break;
+                                        }
+                                        break;
+                                }
+                            }
+                        }
+
+                    }
+
+                    else if (firstLine == HeadBrowser)
+                    {
+                        // Until you get to the end or '[' symbol
+                        while (sr.Peek() != -1 && (Convert.ToChar(sr.Peek()).CompareTo('[') != 0))
+                        {
+                            string line = sr.ReadLine();
+                            if (line != "")
+                            {
+                                string setName = line.Substring(0, 3);
+                                string setValue = line.Substring(4);
+
+                                if (setName == "URL")
+                                {
+                                    this.txtURL.Text = setValue;
+                                    this.wbBrowser.Navigate(this.txtURL.Text);
+                                }
+                            }
+                        }
+                    }
+
+                }
+
+                sr.Close();
+            }
+            // If the file does not exist or the value is incorrect, set the default setting
+            catch { }
+        }
+
+
+        // Key event handler
+        private void txtURL_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            // Check for the flag being set in the KeyDown event.
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                wbBrowser.Navigate(txtURL.Text);
+                e.Handled = true;
+            }
+
+        }
+    }
+}
+
